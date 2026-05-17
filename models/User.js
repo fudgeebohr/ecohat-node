4f020418d2c7bf4ae9b6a740657913c624e33181
Fudge <fugenkenneth@gmail.com>
2026-05-17 10:57:09 +0800

    Fix syntax error in User model by adding missing comma after isArchived field

--- models/User.js
+++ models/User.js
@@ -47,7 +47,7 @@ const UserSchema = new mongoose.Schema({
   isArchived: { 
     type: Boolean, 
     default: false 
-  }
+  },
   cart: [
     {
       itemId: { type: Number, required: true }, // Matches your item.id
