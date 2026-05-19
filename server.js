9e9b91f6d37ac15a5b684093dc4c2e087a3a5a46
Fudge <fugenkenneth@gmail.com>
2026-05-19 15:39:23 +0800

    Increase request body size limit for JSON and URL-encoded data

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use((req, res, next) => {
   console.log(`Incoming Request: ${req.method} ${req.url}`);
